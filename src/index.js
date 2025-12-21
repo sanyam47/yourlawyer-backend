378180991e80b4bcdf298da120e80d52765ea45c
sanayam <sanyam4747@gmail.com>
2025-12-21 18:17:14 +0530

    fix CORS for Vercel frontend

--- src/index.js
+++ src/index.js
@@ -13,7 +13,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: [
+      "http://localhost:3000",
+      "https://yourlawyer.vercel.app",
+    ], credentials: true }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
